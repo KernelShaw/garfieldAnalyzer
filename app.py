20f37b817c606335156426d80d4e00e65151a8c6
tak3369 <98980668+tak3369@users.noreply.github.com>
2022-05-12 17:08:35 -0700

    trying to deploy i hate my life

--- app.py
+++ app.py
@@ -1,5 +1,84 @@
 from flask import Flask, request, render_template, redirect, url_for
-from strip_methods import GarfieldBase
+import pygsheets
+
+
+def drive_update(index, joke, laugh_bool):
+    cell_conversion = joke + str(index + 2)
+    if laugh_bool is None:
+        update_the_master_sheet(cell_conversion)
+    else:
+        update_the_master_sheet(cell_conversion, "N" + str(index + 2))
+    return
+
+
+def update_the_master_sheet(cell, laugh=None):
+    # authorization
+    google_client = pygsheets.authorize(service_file='static/pvt/gardata-test-f05cbfa3dde5.json')
+    master_sheet = google_client.open('Garfield Analysis Sheet')
+    data_sheet = master_sheet[0]
+
+    if laugh is not None:
+        data_sheet.update_value(laugh, int(data_sheet.get_value(cell, 'UNFORMATTED_VALUE')) + 1)
+
+    data_sheet.update_value(cell, int(data_sheet.get_value(cell, 'UNFORMATTED_VALUE')) + 1)
+
+
+class GarfieldBase:
+    def __init__(self):
+        self.strip_info = dict()
+        self.strip_list = list()
+
+        n = open("data/counts.txt", 'r')
+        with open("data/strip_list.txt", 'r') as t:
+            lines = t.readlines()
+            count_list = n.readlines()
+            count_index = 0
+
+            for comic in lines:
+                self.strip_info[(comic.split(".")[0])] = [int(count_list[count_index]), count_index]
+                # So YOU don't forget, this line basically:
+                # DICT [ "strip_date.gif".split(".")[0] -> strip_date
+                # VALUE int(count_list[ same_index_as_comic ]) , count_index
+                count_index = count_index + 1
+        n.close()
+        for comic in lines:
+            self.strip_list.append(comic.split(".")[0])
+
+    def random_strip(self):
+        import random
+        return random.choice(self.strip_list)
+
+    def update_entry_details(self, strip, joke, laugh_bool=None):
+        total_number_of_responses = self.strip_info[strip][0]
+        index = self.strip_info[strip][1]
+
+        n = open("data/counts.txt", 'r')
+        count_list = n.readlines()
+        if len(count_list[index]) == 2:
+            count_list[index] = str(total_number_of_responses + 1) + "\n"
+        else:
+            count_list[index] = str(total_number_of_responses + 1)  # The ONE edge case
+
+        n = open("data/counts.txt", 'w')
+        n.writelines(count_list)
+        n.close()
+
+        if total_number_of_responses + 1 == 10:  # Once a strip reaches 10 responses, it's retired.
+            n = open("data/strip_list.txt", 'r')
+            strips = n.readlines()
+            if len(strips[index]) == 15:
+                strips[index] = "COMPLETED " + strips[index] + "\n"
+            else:
+                strips[index] = "COMPLETED " + strips[index]  # The ONE edge case
+
+            n = open("data/strip_list.txt", 'w')
+            n.writelines(strips)
+            n.close()
+
+            del self.strip_list[index]
+
+        drive_update(index, joke, laugh_bool)
+
 
 # GLOBALS
 RANDOM_STRIP = ""
